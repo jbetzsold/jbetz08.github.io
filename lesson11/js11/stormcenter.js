d26fad47341d97b531e5e35247f4c2c4a47573c1
jbetz08 <bet16011@byui.edu>
2021-03-17 20:42:43 -0600

    storm center update

---
+++ lesson11/js11/stormcenter.js
@@ -0,0 +1,111 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
+
+const now = new Date();
+const today = now.getDay();
+document.querySelector('#daynumber').textContent = now.getDate();
+const month = now.getMonth();
+document.querySelector('#year').textContent = now.getFullYear();
+
+switch (today) {
+    case 0:
+        document.querySelector('#dayofweek').textContent = 'Sunday';
+        break;
+    case 1:
+        document.querySelector('#dayofweek').textContent = 'Monday';
+        break;
+    case 2:
+        document.querySelector('#dayofweek').textContent = 'Tuesday';
+        break;
+    case 3:
+        document.querySelector('#dayofweek').textContent = 'Wednesday';
+        break;
+    case 4:
+        document.querySelector('#dayofweek').textContent = 'Thursday';
+        break;
+    case 5:
+        document.body.onload = addElement;
+        document.querySelector('#dayofweek').textContent = 'Friday';
+        break;
+    case 6:
+        document.querySelector('#dayofweek').textContent = 'Saturday';
+        break;
+    default:
+        document.querySelector('#dayofweek').textContent = 'Something is wrong with day of week';
+}
+
+switch (month) {
+    case 0:
+        document.querySelector('#month').textContent = 'January';
+        break;
+    case 1:
+        document.querySelector('#month').textContent = 'February';
+        break;
+    case 2:
+        document.querySelector('#month').textContent = 'March';
+        break;
+    case 3:
+        document.querySelector('#month').textContent = 'April';
+        break;
+    case 4:
+        document.querySelector('#month').textContent = 'May';
+        break;
+    case 5:
+        document.querySelector('#month').textContent = 'June';
+        break;
+    case 6:
+        document.querySelector('#month').textContent = 'July';
+        break;
+    case 7:
+        document.querySelector('#month').textContent = 'August';
+        break;
+    case 8:
+        document.querySelector('#month').textContent = 'September';
+        break;
+    case 9:
+        document.querySelector('#month').textContent = 'October';
+        break;
+    case 10:
+        document.querySelector('#month').textContent = 'November';
+        break;
+    case 11:
+        document.querySelector('#month').textContent = 'December';
+        break;
+    default:
+        document.querySelector('#month').textContent = 'Something is messed up with month';
+}
+
+
+function addElement () {
+
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "banner");
+
+    const newContent = document.createTextNode("Saturday = Preston Pancakes in the Park! 9:00 a.m.\n Saturday at the city park pavilion.");
+    newDiv.appendChild(newContent);
+
+    const currentDiv = document.getElementById("yes");
+
+    document.body.insertBefore(newDiv, currentDiv);
+
+}
+
+
+WebFont.load({
+    google: {
+        families: [
+            'Oswald'
+        ]
+    }
+});
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
