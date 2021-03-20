54d8b454b84b391d388a673a72c41ab257c393ec
jbetz08 <bet16011@byui.edu>
2021-03-20 11:13:17 -0600

    more updates

--- lesson11/js11/sodasprings.js
+++ lesson11/js11/sodasprings.js
@@ -194,3 +194,41 @@ fetch(forecastAPI)
     });
     
 
+//town events data
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+
+    for (let i = 0; i < towns.length; i++ ) {
+        if (towns[i].name == "Soda Springs") {
+
+            let card = document.createElement('div');
+            let event1 = document.createElement('h4');
+            let event2 = document.createElement('h4');
+            let event3 = document.createElement('h4');            
+
+            event1.textContent = towns[i].events[0];
+            event2.textContent = towns[i].events[1];
+            event3.textContent = towns[i].events[2];
+
+            card.appendChild(event1);
+            card.appendChild(event2);
+            card.appendChild(event3);
+
+            document.querySelector('section.events').appendChild(card);
+
+        }
+
+
+    }
+
+    
+
+  });
+
