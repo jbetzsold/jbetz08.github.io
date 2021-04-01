5dacfe02ee6fc94f64ea589e3866f419bb421ef7
jbetz08 <bet16011@byui.edu>
2021-03-31 19:15:07 -0600

    finished index page

--- finalProject/js/scripts.js
+++ finalProject/js/scripts.js
@@ -20,6 +20,32 @@ const loadImages = (image) => {
   };
 };
 
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+// MDN Web Docs
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+    
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+
 // js dates
 const now = new Date();
 const today = now.getDay();
