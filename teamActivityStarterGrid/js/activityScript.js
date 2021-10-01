62bbbba732a7437b8babe5b44c6eb1343aec38aa
Josh Betzsold <bet16011@byui.edu>
2021-09-30 18:22:13 -0600

    team activity update

--- teamActivityStarterGrid/js/activityScript.js
+++ teamActivityStarterGrid/js/activityScript.js
@@ -1,6 +1,34 @@
 document.getElementById("bpricing").style.fontSize = "x-large";
 
-function delayCard1(){
-    
-
+function showDiv1() {
+    setTimeout(function () {
+        document.getElementById("card1").style.visibility = "visible";
+    }, 1000);
 }
+showDiv1();
+function showDiv2() {
+  setTimeout(function () {
+      document.getElementById("card2").style.visibility = "visible";
+  }, 2000);
+}
+showDiv2();
+function showDiv3() {
+  setTimeout(function () {
+      document.getElementById("card3").style.visibility = "visible";
+  }, 3000);
+}
+showDiv3();
+
+color = '#FFCF2D'
+document.querySelector('.popular').style.backgroundColor = color;
+
+y = document.getElementsByTagName('a');
+for (let i = 0; i < y.length; i++) {
+    y[i].style.backgroundColor = '#FFCF2D';
+}
+
+function showAlert() {
+    var myText = "You've signed in!";
+    alert (myText);
+}
+
