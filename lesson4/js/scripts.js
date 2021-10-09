6e25fd96590ccda6d6b55fdd09596e787782314b
Josh Betzsold <bet16011@byui.edu>
2021-10-09 15:12:30 -0600

    week 4 updates

--- lesson4/js/scripts.js
+++ lesson4/js/scripts.js
@@ -80,3 +80,18 @@ switch (month) {
     default:
         document.querySelector('#month').textContent = 'Something is messed up with month';
 }
+
+
+const menuButton = document.getElementById("menu-icon");
+const mainnav = document.querySelector(".main-nav");
+menuButton.addEventListener(
+    "click",
+    () => {
+        mainnav.classList.toggle("expanded");
+    },
+    false
+);
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 760) mainnav.classList.remove("expanded");
+};
