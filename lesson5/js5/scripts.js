fc449583861c731526ed28abdacef267d2a71ba8
Josh Betzsold <bet16011@byui.edu>
2021-10-12 12:37:33 -0600

    changes

--- lesson5/js5/scripts.js
+++ lesson5/js5/scripts.js
@@ -96,3 +96,18 @@ function addElement () {
     document.body.insertBefore(newDiv, currentDiv);
 
 }
+
+
+const menuButton = document.getElementById("menu-icon");
+const mainnav = document.querySelector(".main-nav");
+menuButton.addEventListener(
+    "click",
+    () => {
+        mainnav.classList.toggle("expanded");
+    },
+    false
+);
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {
+    if (window.innerWidth > 760) mainnav.classList.remove("expanded");
+};
