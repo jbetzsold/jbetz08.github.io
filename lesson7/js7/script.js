c00a636d9b0068ae5ead3dd401c4aeb1d73be85d
Josh Betzsold <bet16011@byui.edu>
2021-10-24 20:57:40 -0600

    img updates

--- lesson7/js7/script.js
+++ lesson7/js7/script.js
@@ -147,27 +147,3 @@ hambutton.addEventListener('click',() => {mainnav.classList.toggle('responsive')
 
 window.onresize =() => {if(window.innerWidth > 643) mainnav.classList.remove('responsive')};
 
-
-// windspeed
-var windSpeed = document.getElementById("windspeed").innerText;
-var temperature = document.getElementById("temp").innerText;
-var wc = document.getElementById("windchill");
-
-function calcWindChill (windSpeed, temperature) {
-    let windChill = 35.74 + (0.6215 * temperature) - (35.75 * (Math.pow(windSpeed, 0.16))) + (0.4275 * temperature) * Math.pow(windSpeed, 0.16);
-
-    if (temperature <= 50 && windSpeed > 3) {
-        return Math.round(windChill);
-    }
-
-    else {
-        return "N/A";
-    }
-}
-
-wc.innerHTML = calcWindChill(windSpeed, temperature);
-
-console.log(calcWindChill(windSpeed, temperature))
-
-
-
