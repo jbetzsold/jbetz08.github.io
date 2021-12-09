69a505ea16fcdcd908b17d4d014bec0a7dfcaf11
Josh Betzsold <bet16011@byui.edu>
2021-12-08 18:21:56 -0700

    going live for testing

--- finalProject/js/realtimeDB.js
+++ finalProject/js/realtimeDB.js
@@ -1,2 +1,20 @@
 // DB updates
 
+
+var firstName = document.getElementById('fname').value;
+var lastName = document.getElementById('lname').value;
+var email = document.getElementById('email').value;
+var businessName = document.getElementById('businessName').value;
+
+
+import { getDatabase, ref, set } from "firebase/database";
+
+function writeUserData(firstName, lastName, email, businessName) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    first_name: firstName,
+    last_name: lastName,
+    email: email,
+    business: businessName
+  });
+}
