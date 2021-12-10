6440c0c8b96073cac3d62fc300c9ce71e9de430f
Josh Betzsold <bet16011@byui.edu>
2021-12-10 10:56:54 -0700

    updates

--- finalProject/js/realtimeDB.js
+++ finalProject/js/realtimeDB.js
@@ -47,7 +47,7 @@ button.addEventListener('click', saveData);
 
 function writeUserData(data) {
   const db = getDatabase();
-  const newPostRef = push(ref(db, 'users/' + 5));
+  const newPostRef = push(ref(db, 'users'));
   set(newPostRef, data);
 }
 
